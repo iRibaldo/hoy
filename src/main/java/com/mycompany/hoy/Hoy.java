81b4a0a7c5cf76cb0bbb02039c906a9eb4a708ad
iRibaldo <ribaldo.nantipbarre3a@gmail.com>
2024-10-30 11:53:55 -0500

    hola

---
+++ src/main/java/com/mycompany/hoy/Hoy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hoy;
+
+/**
+ *
+ * @author User
+ */
+public class Hoy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
